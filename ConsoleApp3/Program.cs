434e706602f0209d056bc54d880bf63614e3abd5
TooMach0 <donimichal@gmail.com>
2023-06-24 17:25:45 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace ConsoleApp
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+            
+            string player1 = Console.ReadLine();
+            string player2 = Console.ReadLine();
+
+           Console.WriteLine(Rps(player1, player2));
+
+        }
+
+        public static string Rps(string p1, string p2)
+        {
+            string[] KamenPapierNoznice = new string[] { "rock", "paper", "scissors" };
+
+            for (int i = 0; i < KamenPapierNoznice.Length; i++)
+            {
+                p1.Equals(KamenPapierNoznice[i], StringComparison.OrdinalIgnoreCase);
+                p2.Equals(KamenPapierNoznice[i], StringComparison.OrdinalIgnoreCase);
+            }
+            
+
+            if (p1 == "scissors" && p2 == "paper")
+            {
+
+                return "player1 wins";
+            }
+            else if (p1 == "paper" && p2 == "rock")
+            {
+
+                return "player1 wins";
+            }
+            else if (p1 == "rock" && p2 == "scissors")
+            {
+                return "player1 wins";
+            }
+            else
+            {
+                return "player2 wins";
+            }
+        }
+    }
+}
